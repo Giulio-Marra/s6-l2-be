61d99ae8cf32b980411069dc3bee967b89699d2a
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-25 12:04:18 +0200

    base progetto

---
+++ s6-l2-be/src/main/java/giulio_marra/s6_l2_be/S6L2BeApplication.java
@@ -0,0 +1,13 @@
+package giulio_marra.s6_l2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6L2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6L2BeApplication.class, args);
+	}
+
+}
---
+++ s6-l2-be/src/test/java/giulio_marra/s6_l2_be/S6L2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package giulio_marra.s6_l2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6L2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
